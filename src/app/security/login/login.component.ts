6b368fcb4e0b2fb09e078700dcac242e3e92ce16
rafa2892 <rafav2009@gmail.com>
2024-08-02 11:10:24 +0200

    avances front seguridad

--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserAuth } from '../user-auth';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
-}
+  userLogin:UserAuth;
+
+  
+  constructor(private authServicio:AuthService){}
+
+  // constructor(
+  //   private viajeServicio:ViajeServicioService,private router:Router, private carroServicio:CarroService,
+  //   private _snackBar: MatSnackBar,public dialog: MatDialog, private conductorService:ConductorServiceService,
+  //   private rutaServicio:RutasService, private cdr: ChangeDetectorRef){}
+
+
+  ngOnInit(): void {
+    this.userLogin = new UserAuth();
+  }
+
+  onSubmit(){
+
+    this.authServicio.login(this.userLogin);
+        this.authServicio.login(this.userLogin).subscribe(dato => {
+        });
+
+
+
+      }
+  }
+
---
+++ src/app/security/user-auth.ts
@@ -0,0 +1,6 @@
+export class UserAuth {
+
+    clave:string;
+    usuario:string;
+
+}
