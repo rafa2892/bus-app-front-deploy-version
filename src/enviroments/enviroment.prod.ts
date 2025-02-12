9596d21cd807395348fe41166f212faebc7e5fdd
rafa2892 <rafav2009@gmail.com>
2025-02-12 18:29:47 +0100

    avances

---
+++ src/enviroments/enviroment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,  // Indica que es un entorno de producción
+    apiUrl: 'https://tu-dominio.com/api/v1'  // URL del backend en producción
+};
---
+++ src/enviroments/enviroment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,  // Indica que es un entorno de desarrollo
+    apiUrl: 'http://localhost:8080/api/v1'  // URL del backend en desarrollo
+};
