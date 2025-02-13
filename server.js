d0c5ac70986896b6acb76d859ba472a875aec16b
rafa2892 <rafav2009@gmail.com>
2025-02-13 12:35:24 +0100

    config deployment

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/gestion-contador-frontend-no-standalone'));
+app.use(express.static(path.join(__dirname, 'dist', 'gestion-contador-frontend-no-standalone')));
 
+// Handle all routes by sending the index.html file
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/gestion-contador-frontend-no-standalone/'}),
+    res.sendFile('index.html', {root: path.join(__dirname, 'dist', 'gestion-contador-frontend-no-standalone')}),
 );
 
 // Start the app by listening on the default Heroku port
