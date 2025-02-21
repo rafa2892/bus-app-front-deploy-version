4431a67f949a0fb2abc2b5a4423c2b686ef7d71f
rafa2892 <rafav2009@gmail.com>
2025-02-21 14:33:38 +0100

    environment fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,  // Indica que es un entorno de producción
     // apiUrl: 'https://tu-dominio.com/api/v1'  // URL del backend en producción
-    apiUrl: 'https://bus-app-back-9a61b7d383e6.herokuapp.com/api/v1'
+    apiUrl: 'https://bus-app-back-e2502c4e8d95.herokuapp.com/api/v1'
 };
